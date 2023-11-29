5f7bbc697fca0e30759c2cf0c623ad0b90b78f5d
Guilherme Monte-Mor <guimontemor777@gmail.com>
2023-11-29 17:12:45 -0300

    Fixed Bilioteca's name

---
+++ Bilioteca.Model/Repositories/RepositoryAutor.cs
@@ -0,0 +1,26 @@
+﻿using Bilioteca.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bilioteca.Model.Repositories
+{
+    public class RepositoryAutor : RepositoryBase<RepositoryAutor>
+    {
+        public RepositoryAutor(bool saveChanges= true) : base(saveChanges) 
+        {
+
+        }
+
+        public async Task IncluirAsync(Nome Nome, Nacionalidade Nacionalidade, Livros)
+        {
+            _context.Entry(cliente).State = Microsoft.EntityFrameworkCore.EntityState.Added;
+            await _context.SaveChangesAsync();
+
+            endereco.EndCodigoCliente = cliente.CliCodigo;
+            _context.Entry(endereco).State = Microsoft.EntityFrameworkCore.EntityState.Added;
+            await _context.SaveChangesAsync();
+        }
+    }
